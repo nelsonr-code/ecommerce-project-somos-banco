1b4990cb86cf26cc46dfa9efb4c95e644f81302e
Nelson Rivas <nelson.rivas.ucv@gmail.com>
2021-07-10 15:23:26 -0400

    init config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import './assets/styles/index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <h2>Hello</h2>
+    </div>
+  );
+}
+
+export default App;
