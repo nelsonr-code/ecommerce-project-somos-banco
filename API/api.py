3d4b9f960cb15eba6e81bf439e37d45ca7f68705
Diego Branco <diegoh89@gmail.com>
2021-07-23 17:06:07 -0400

    Ahora se guarda el hash de los password en lugar del password en si. Usuarios anteriores no sirven.

--- API/api.py
+++ API/api.py
@@ -1,80 +1,85 @@
 from flask import Flask, jsonify, request
 import string, psycopg2
+from passlib.context import CryptContext
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-
 msg_banco: string
 msg_banco = "<h1> SOMOS BANCO </h1> <p> TU GENTE, TU BANCO </p>"
+key = app.config.get('ENCRYPTION_KEY')
+
+pwd_context = CryptContext(schemes=["pbkdf2_sha256"])
+
 
 def connectDatabase():
     global conn
     try:
         conn = psycopg2.connect(
-            host = app.config.get('DATABASE_HOST'),
-            database = app.config.get('DATABASE_NAME'),
-            user = app.config.get('DATABASE_USER'),
-            password = app.config.get('DATABASE_PSSWD')
-    )
+                host = app.config.get('DATABASE_HOST'),
+                database = app.config.get('DATABASE_NAME'),
+                user = app.config.get('DATABASE_USER'),
+                password = app.config.get('DATABASE_PSSWD')
+        )
         print('successful connection to the database!')
+        global db
+        db = conn.cursor()
     except Exception as error:
         print('error connecting to database', error)
     #cursor para ejecutar los query
-    global db
-    db = conn.cursor()
+    
 
 connectDatabase()
 
-@app.route('/', methods=['GET']) #home
+@app.route('/api/v1', methods=['GET']) #home
 def home():
     return msg_banco
 
-@app.route('/login', methods=['POST']) #Log In
+@app.route('/api/v1/login', methods=['POST']) #Log In
 def login():
     json = request.get_json(force=True)
     email = json['login']
     passw = json['password']
-    sql = """   SELECT id_customer 
+    #print("PASSW: "+passw)
+    sql = """   SELECT id_customer, password 
                 FROM tblcustomer as c
-                WHERE (c.customer_email, c.password) = (%(email)s,%(passw)s) """
+                WHERE (c.customer_email) = (%(email)s) """
     try:
         db.execute(sql, {'email':email, 'passw':passw})
         result = db.fetchone()
+        print(result)
         if(result):
-            #usuario y password coinciden
-            response = {"user_id":result[0]}
-            return jsonify(response)
-        else: 
-            #chequeamos si ese usuario existe
-            sql = """ SELECT id_customer
-                    FROM tblcustomer as c
-                    WHERE (c.customer_email) = (%(email)s) """
-            db.execute(sql, {'email':email})
-            result = db.fetchone()
-            if(result):
+            #procedemos a revisar el password
+            passwHash = result[1]
+            if(pwd_context.verify(passw,passwHash)):
+                #contrasena coincide
+                response = {"user_id":result[0]}
+                return jsonify(response)
+            else:
                 #el usuario existe, pero la contrasenha no es correcta. se le notifica al usuario.
                 return jsonify({'message':'Wrong Password'}), 403
-            else:
+        else: 
                 #el usuario no existe, se deberia notificar al usuario para que se registre
-                return jsonify({'message':'Wrong username'}), 403
+                return jsonify({'message':'Wrong Username'}), 403
     except Exception as e:
         print("error at /login:")
         print (e)
         connectDatabase()
 
 
-@app.route('/registrar', methods=['POST']) #registrar usuario
+@app.route('/api/v1/registrar', methods=['POST']) #registrar usuario
 def register():
     json = request.get_json(force=True)
     email = json['email']
     passw = json['password']
+    passwHash=pwd_context.hash(passw)
+    #print("PASSW: "+passw)
     name = json['name']
     phone = json['phone']
     sql = """INSERT INTO  tblcustomer (id_customer,name_customer,customer_phone,customer_email,password)
              VALUES (DEFAULT, %(name)s,%(phone)s,%(email)s,%(passw)s)             
     """
     try:
-        db.execute(sql, {'email':email, 'passw':passw, 'name':name, 'phone':phone})
+        db.execute(sql, {'email':email, 'passw':passwHash, 'name':name, 'phone':phone})
         conn.commit()
         #sql = """ SELECT * FROM tblcustomer 
         #          ORDER BY id_customer DESC
@@ -90,11 +95,11 @@ def register():
         connectDatabase()
 
 
-@app.route('/consultar', methods=['GET']) #Consultar Cuentas (clientes y TCred)
+@app.route('/api/v1/consultar', methods=['GET']) #Consultar Cuentas (clientes y TCred)
 def consultar():
-    return msg_banco
+    return jsonify(key)
 
-@app.route('/card/pay', methods=['POST']) #pagar tarjeta de credito
+@app.route('/api/v1/card/pay', methods=['POST']) #pagar tarjeta de credito
 def pagar():
     return jsonify({
         'message': 'xxxxxxxxxxxx',
@@ -102,11 +107,14 @@ def pagar():
         'orden': 'x-44512'
         })  
 
-@app.route('/movimientos', methods=['GET']) #movimientos de cuentas 
+@app.route('/api/v1/movimientos', methods=['GET']) #movimientos de cuentas 
 def movimientos():
     return msg_banco
 
 #faltan tambien los de uso interno de la app
 if __name__ == '__main__':
-    db.close()
-    conn.close()
+    app.run(port=3001)
+    
+db.close()
+conn.close()
+print("API Closed")
--- API/config.py
+++ API/config.py
@@ -1,9 +1,13 @@
-from os import environ
+import os
+from dotenv import load_dotenv
+BASE_DIR = os.path.join( os.path.dirname( __file__ ), '..' )
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
-DATABASE_URL = environ.get('DATABASE_URL')
-DATABASE_USER = environ.get('DATABASE_USER')
-DATABASE_PSSWD = environ.get('DATABASE_PSSWD')
-DATABASE_HOST = environ.get('DATABASE_HOST')
-DATABASE_PORT = environ.get('DATABASE_PORT')
-DATABASE_NAME = environ.get('DATABASE_NAME')
-DATABASE_URL= environ.get('DATABASE_NAME')
+DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_USER = os.getenv('DATABASE_USER')
+DATABASE_PSSWD = os.getenv('DATABASE_PSSWD')
+DATABASE_HOST = os.getenv('DATABASE_HOST')
+DATABASE_PORT = os.getenv('DATABASE_PORT')
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+DATABASE_URL= os.getenv('DATABASE_NAME')
+
